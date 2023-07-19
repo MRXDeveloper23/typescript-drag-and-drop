292f342ab802b31a3b5f7288ea8b42c77b85a218
Khurmatillo <khurmatillomamasoliev@gmail.com>
2023-07-19 16:44:51 +0500

    drag & drop interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Drag and drop intefaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 enum ProjectStatus {
   Active,
   Finished,
