ac64be839b0d525be5ff346206162aeb230b13bf
Khurmatillo <khurmatillomamasoliev@gmail.com>
2023-07-20 17:39:08 +0500

    removing .js file extensions

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { Component } from "./base-component.js";
-import { Validatable, validate } from "../utils/validation.js";
-import { Autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
+import { Component } from "./base-component";
+import { Validatable, validate } from "../utils/validation";
+import { Autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Component } from "./base-component.js";
-import { Project } from "../models/project.js";
-import { Draggable } from "../models/drag-drop.js";
-import { Autobind } from "../decorators/autobind.js";
+import { Component } from "./base-component";
+import { Project } from "../models/project";
+import { Draggable } from "../models/drag-drop";
+import { Autobind } from "../decorators/autobind";
 
 export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
   private project: Project;
